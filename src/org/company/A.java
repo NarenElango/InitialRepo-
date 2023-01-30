486e9ffae64fb1b433af59bb51458f7f36b7199b
NarenElango <narennsmsd@gmail.com>
2023-01-30 16:42:18 +0530

    initial commit

---
+++ src/org/company/A.java
@@ -0,0 +1,11 @@
+package org.company;
+
+public class A extends B {
+	public A() {		
+		System.out.println("Default constructor");
+	}
+	
+	public static void main(String[] args) {
+		A a= new A();
+	}
+}
---
+++ src/org/company/B.java
@@ -0,0 +1,11 @@
+package org.company;
+
+public class B {
+	public B() {
+		System.out.println("super class");
+	}
+	public B(int id) {
+		System.out.println("cus id"+id);
+	}
+
+}
